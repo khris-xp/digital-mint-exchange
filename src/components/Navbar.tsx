388b8f2b5163c4dcc5040e0de73c309b114f451e
khris-xp <khrisbharmmano@gmail.com>
2023-12-21 12:51:15 +0700

    style(svg): remove stroke line and stroke width

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,13 +15,13 @@ export default function Navbar() {
                     </a>
                     <div className="flex md:hidden">
                         <button onClick={handleOpenNavbar} type="button" className="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600" aria-label="toggle menu">
-                            <svg xmlns="http://www.w3.org/2000/svg" className={`w-6 h-6 ${isOpen && 'hidden'}`} fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M4 8h16M4 16h16" />
+                            <svg xmlns="http://www.w3.org/2000/svg" className={`w-6 h-6 ${isOpen && 'hidden'}`} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M4 8h16M4 16h16" />
                             </svg>
 
                             <svg xmlns="http://www.w3.org/2000/svg" className={`w-6 h-6 ${!isOpen && 'hidden'}`} fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                                stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                strokeWidth="2">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                             </svg>
                         </button>
                     </div>
